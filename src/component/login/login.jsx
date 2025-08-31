f9ff2c689862c09e5c4d23a4803496d8c73bc12a
zena-ahmed <zenaahmedsaad928@gmail.com>
2025-08-31 17:00:41 +0300

    added Login page and merged latest changes from main

--- src/component/login/login.jsx
+++ src/component/login/login.jsx
@@ -1,9 +1,34 @@
-import React from 'react'
-import './login.css'
-const login = () => {
-  return (
-    <div>login</div>
-  )
-}
 
-export default login
+import React from 'react';
+import './login.css';
+const Login = () => {
+  return (
+    <div className="login">
+      <div className="login-img">
+        <img src="/public/loginimage.jpg" alt="loginimg"/>
+      </div>
+        <div className="login-container">
+            <p >  Log in to Exclusive</p>
+            <p>Enter your details below</p>
+            <form>
+                <input type="email" required placeholder='Email or Phone Number' />
+                <input type="password" required placeholder='Password'/>
+                
+          <div className="login-actions">
+          <button type="submit" className="login-btn">
+            Log In
+          </button>
+            <a href="/forgot-password" className="forgot-link">
+              Forgot Password?
+            </a>
+              </div>
+            </form>
+            
+        
+       
+        </div>
+    </div>
+  );
+};
+
+export default Login;
