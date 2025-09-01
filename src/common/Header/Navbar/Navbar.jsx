0827625d7df9d6bc2b30708c0522493c37b15f89
Noura Mohamed <noramohamedibrahim19@gmail.com>
2025-09-01 09:26:45 +0300

    Responsive updates for Navbar and Head section

--- src/common/Header/Navbar/Navbar.jsx
+++ src/common/Header/Navbar/Navbar.jsx
@@ -1,54 +1,58 @@
-import React from "react";
+import React, { useState } from "react";
 import { NavLink } from "react-router-dom";
 import "./navbar.css";
 import { FaSearch } from "react-icons/fa";
 import { FaCartShopping } from "react-icons/fa6";
 import { MdFavoriteBorder } from "react-icons/md";
 import { FaUserAlt } from "react-icons/fa";
-import logo from "./../../../assets/Logo.png";
+import logo from "./../../../assets/images/Logo.png";
+import { HiDotsVertical } from "react-icons/hi";
+
 const Navbar = () => {
+  const [menuOpen, setMenuOpen] = useState(false);
+  const toggleMenu = () => setMenuOpen(!menuOpen);
+
   return (
     <div className="Navbar">
       <nav>
         <div className="logo">
-          <img src={logo} alt="" />
+          <img src={logo} alt="Logo" />
+          <button className="menu-toggle" onClick={toggleMenu}>
+            <HiDotsVertical />
+          </button>
         </div>
 
-        <ul>
+        <ul className={menuOpen ? "active" : ""}>
           <li>
-            <NavLink to={"/"} end aria-current="page">
+            <NavLink to="/" end>
               Home
             </NavLink>
           </li>
           <li>
-            <NavLink to={"/contact"}>Contact</NavLink>
+            <NavLink to="/contact">Contact</NavLink>
           </li>
           <li>
-            <NavLink to={"/about"}>About</NavLink>
+            <NavLink to="/about">About</NavLink>
           </li>
-
           <li>
-            <NavLink to={"/signup"}>Signup</NavLink>
+            <NavLink to="/signup">Signup</NavLink>
           </li>
         </ul>
 
         <div className="search-cart-wishlist">
           <div className="search">
             <input type="text" placeholder="What are you looking for?" />
-
             <FaSearch className="search-icon" />
           </div>
 
           <div className="icons-page">
-            <NavLink to={"/cart"}>
+            <NavLink to="/cart">
               <FaCartShopping />
             </NavLink>
-
-            <NavLink to={"/wishlist"}>
+            <NavLink to="/wishlist">
               <MdFavoriteBorder />
             </NavLink>
-
-            <NavLink to={"/login"}>
+            <NavLink to="/login">
               <FaUserAlt />
             </NavLink>
           </div>
---
+++ src/component/heading/sidebar/Sidebar.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import './sidebar.css'
+const Sidebar = () => {
+  return (
+    <div className="Sidebar">
+      <div className="container">
+        <ul>
+          <li>Phones</li>
+          <li>Computers</li>
+          <li>SmartWatch</li>
+          <li>Camera</li>
+          <li>HeadPhones</li>
+          <li>Gaming</li>
+        </ul>
+      </div>
+    </div>
+  );
+}
+
+export default Sidebar
